874c0bdc642738f60079db3c12d953e97360e59f
proyecto <andresgc1213@gmail.com>
2018-10-14 16:17:27 -0500

    0.0

--- Segunparcial_App/Segunparcial_App.iOS/AppDelegate.cs
+++ Segunparcial_App/Segunparcial_App.iOS/AppDelegate.cs
@@ -20,6 +20,7 @@ namespace Segunparcial_App.iOS
         //
         // You have 17 seconds to return from this method, or iOS will terminate your application.
         //
+        //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
